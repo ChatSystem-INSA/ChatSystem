0a6641d8f31694c0b852bf9dc9a566968b6fec83
thetlk <jeremieboutoille@gmail.com>
2014-11-27 17:48:19 +0100

    TcpListener now extends Thread

--- src/main/java/com/insatoulouse/chatsystem/ni/TcpListener.java
+++ src/main/java/com/insatoulouse/chatsystem/ni/TcpListener.java
@@ -5,12 +5,17 @@ import java.io.IOException;
 /**
  * Created by tlk on 27/11/14.
  */
-public class TcpListener {
+public class TcpListener extends Thread {
 
     private TcpSocket socket;
 
     public TcpListener() throws IOException {
         this.socket = TcpSocket.getInstance();
+    }
+
+    public void run()
+    {
+        while(true){}
     }
 
 }
