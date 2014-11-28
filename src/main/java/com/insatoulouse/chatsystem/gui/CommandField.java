cb11b8453eea4693042b04ec74d3a950da701941
David Livet <david.livet@orange.fr>
2014-11-28 16:10:31 +0100

    Add historic command

--- src/main/java/com/insatoulouse/chatsystem/gui/CommandField.java
+++ src/main/java/com/insatoulouse/chatsystem/gui/CommandField.java
@@ -7,9 +7,13 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
 
 
 public class CommandField extends JTextField {
+    private ArrayList<String> historyCommand = new ArrayList<String>();
+    private int index = 0;
+
     public CommandField(final ChatGUI chatGUI) {
         this.setBorder(BorderFactory.createCompoundBorder(
                 this.getBorder(),
@@ -18,22 +22,56 @@ public class CommandField extends JTextField {
         this.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                CommandField.this.setEditable(false);
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        if(!CommandField.this.getText().isEmpty()) {
-                            chatGUI.executeCommand(CommandField.this.getText());
-                        }
-                        SwingUtilities.invokeLater(new Runnable() {
-                            @Override
-                            public void run() {
-                                CommandField.this.setText("");
-                                CommandField.this.setEditable(true);
+                if(CommandField.this.isEditable()){
+                    CommandField.this.setEditable(false);
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            if(!CommandField.this.getText().isEmpty()) {
+                                String text = CommandField.this.getText();
+                                chatGUI.executeCommand(text);
+                                historyCommand.add(text);
+                                index = historyCommand.size()-1;
                             }
-                        });
-                    }
-                }).start();
+                            SwingUtilities.invokeLater(new Runnable() {
+                                @Override
+                                public void run() {
+                                    CommandField.this.setText("");
+                                    CommandField.this.setEditable(true);
+                                }
+                            });
+                        }
+                    }).start();
+                }
+            }
+        });
+        this.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                switch (e.getKeyCode()){
+                    case KeyEvent.VK_UP:
+                        CommandField.this.setText(historyCommand.get(index));
+                        if(index>0)
+                            index--;
+                        break;
+                    case KeyEvent.VK_DOWN:
+                        if(index == historyCommand.size())
+                            CommandField.this.setText("");
+                        else
+                            CommandField.this.setText(historyCommand.get(index));
+                        if(index<historyCommand.size())
+                            index++;
+                        break;
+                }
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+
             }
         });
     }
