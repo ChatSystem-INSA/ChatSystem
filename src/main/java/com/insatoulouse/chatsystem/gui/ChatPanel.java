5cf4b7d37b3cd580c130a00aafdfde0c99c6638a
thetlk <jeremieboutoille@gmail.com>
2014-11-28 16:53:28 +0100

    Add scrollbar on bottom

--- src/main/java/com/insatoulouse/chatsystem/gui/ChatPanel.java
+++ src/main/java/com/insatoulouse/chatsystem/gui/ChatPanel.java
@@ -4,6 +4,8 @@ import com.insatoulouse.chatsystem.model.Message;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.AdjustmentEvent;
+import java.awt.event.AdjustmentListener;
 
 public class ChatPanel extends JScrollPane {
 
@@ -15,5 +17,11 @@ public class ChatPanel extends JScrollPane {
 
         this.setViewportView(messageList);
         this.setPreferredSize(new Dimension(600, 350));
+        this.getVerticalScrollBar().addAdjustmentListener(new AdjustmentListener(){
+            public void adjustmentValueChanged(AdjustmentEvent e){
+                e.getAdjustable().setValue(e.getAdjustable().getMaximum());
+            }
+        });
+
     }
 }
