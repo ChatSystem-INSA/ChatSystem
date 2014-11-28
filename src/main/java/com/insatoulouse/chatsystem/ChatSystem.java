06d37b0596dea2ad5c35b3f82e9e02a6782feeb5
David Livet <david.livet@orange.fr>
2014-11-28 10:55:02 +0100

    Add config file and class utils

--- src/main/java/com/insatoulouse/chatsystem/ChatSystem.java
+++ src/main/java/com/insatoulouse/chatsystem/ChatSystem.java
@@ -1,17 +1,27 @@
 package com.insatoulouse.chatsystem;
 
+import com.insatoulouse.chatsystem.exception.TechnicalException;
 import com.insatoulouse.chatsystem.gui.ChatGUI;
 import com.insatoulouse.chatsystem.ni.ChatNI;
+import com.insatoulouse.chatsystem.utils.Config;
 
 import java.io.IOException;
 
 public class ChatSystem {
 
+
     public ChatSystem()
     {
+
         Controller c = new Controller();
         ChatGUI gui = new ChatGUI(c);
-        ChatNI ni = new ChatNI(c,1337);
+        try {
+            Config config = Config.getInstance();
+            String port = config.getProperties("port");
+            ChatNI ni = new ChatNI(c,Integer.parseInt(port));
+        } catch (TechnicalException e) {
+            //TODO informer le GUI du problème
+        }
     }
 
 }
