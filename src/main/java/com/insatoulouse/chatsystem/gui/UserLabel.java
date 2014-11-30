571c39b97ca39f74142505fe936c3a428db5787d
thetlk <jeremieboutoille@gmail.com>
2014-11-30 13:33:29 +0100

    Userlist OK

--- src/main/java/com/insatoulouse/chatsystem/gui/UserLabel.java
+++ src/main/java/com/insatoulouse/chatsystem/gui/UserLabel.java
@@ -4,24 +4,26 @@ import com.insatoulouse.chatsystem.model.User;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 /**
  * Created by david on 27/11/14.
  */
 public class UserLabel extends JPanel implements ListCellRenderer<User> {
+
+    public static final Color colors[] = {Color.GREEN, Color.RED, Color.BLUE};
+
     @Override
     public Component getListCellRendererComponent(JList<? extends User> list, User value, int index, boolean isSelected, boolean cellHasFocus) {
-        //this.setLayout(new BorderLayout());
-        // this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-        // this.setAlignmentX(JPanel.LEFT_ALIGNMENT);
-
-         this.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        this.setLayout(new BorderLayout());
+        this.setBorder(BorderFactory.createEmptyBorder(10, 10, 0, 10));
+        this.setBackground(Color.WHITE);
         // this.setBou
 
         JPanel milieu = new JPanel();
         milieu.setLayout(new BoxLayout(milieu, BoxLayout.Y_AXIS));
         milieu.setAlignmentX(JPanel.LEFT_ALIGNMENT);
-        milieu.setBackground(Color.DARK_GRAY);
+        milieu.setBackground(UserLabel.colors[index%UserLabel.colors.length]);
         milieu.setBorder(BorderFactory.createEmptyBorder(2, 10, 2, 10));
 
         JLabel name = new JLabel(value.getName());
@@ -36,7 +38,7 @@ public class UserLabel extends JPanel implements ListCellRenderer<User> {
         ip.setForeground(Color.white);
         milieu.add(ip);
 
-        this.add(milieu);
+        this.add(milieu, BorderLayout.CENTER);
 
         return this;
     }
