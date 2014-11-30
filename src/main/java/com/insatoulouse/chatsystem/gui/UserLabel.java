39718c98a0bc50b6bd2e956efced17f3b7ba0be4
thetlk <jeremieboutoille@gmail.com>
2014-11-30 17:01:01 +0100

    Update userlist design

--- src/main/java/com/insatoulouse/chatsystem/gui/UserLabel.java
+++ src/main/java/com/insatoulouse/chatsystem/gui/UserLabel.java
@@ -14,28 +14,29 @@ public class UserLabel extends JPanel implements ListCellRenderer<User> {
     private JLabel name = new JLabel();
     private JLabel ip = new JLabel();
     private JPanel milieu = new JPanel();
-    public static final Color colors[] = {Color.GREEN, Color.RED, Color.BLUE};
+    public static final Color colors[] = {
+            new Color(56, 118, 29), // Vert
+            new Color(166, 28, 0), // Rouge
+            new Color(28, 69, 135) // Bleu
+    };
 
     public UserLabel() {
 
         this.setLayout(new BorderLayout());
-        // this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-        // this.setAlignmentX(JPanel.LEFT_ALIGNMENT);
         this.setBackground(Color.WHITE);
-        this.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-        // this.setBou
+        this.setBorder(BorderFactory.createEmptyBorder(10, 10, 0, 10));
 
         milieu.setLayout(new BoxLayout(milieu, BoxLayout.Y_AXIS));
         milieu.setAlignmentX(JPanel.LEFT_ALIGNMENT);
-        milieu.setBorder(BorderFactory.createEmptyBorder(2, 10, 2, 10));
+        milieu.setBorder(BorderFactory.createEmptyBorder(5, 10, 5, 10));
 
         name.setAlignmentX(JPanel.LEFT_ALIGNMENT);
-        name.setFont(new Font("Sans serif", Font.BOLD, 12));
+        name.setFont(new Font("Arial", Font.BOLD, 12));
         name.setForeground(Color.white);
         milieu.add(name);
 
         ip.setAlignmentX(JPanel.LEFT_ALIGNMENT);
-        ip.setFont(new Font("Sans serif", Font.BOLD, 11));
+        ip.setFont(new Font("Arial", Font.PLAIN, 11));
         ip.setForeground(Color.white);
         milieu.add(ip);
 
@@ -44,6 +45,7 @@ public class UserLabel extends JPanel implements ListCellRenderer<User> {
 
     @Override
     public Component getListCellRendererComponent(JList<? extends User> list, User value, int index, boolean isSelected, boolean cellHasFocus) {
+
         name.setText(value.getName());
         ip.setText(value.getIp().toString());
         milieu.setBackground(UserLabel.colors[index%UserLabel.colors.length]);
