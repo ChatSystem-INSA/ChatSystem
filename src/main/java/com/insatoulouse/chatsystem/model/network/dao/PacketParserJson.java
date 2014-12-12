6127a9886e8ead9db9fcd6d01e0ad6badff629d2
David LIVET <david.livet@orange.fr>
2014-12-12 14:04:56 +0100

    Fix  JSONParser drop packet when more fields

--- src/main/java/com/insatoulouse/chatsystem/model/network/dao/PacketParserJson.java
+++ src/main/java/com/insatoulouse/chatsystem/model/network/dao/PacketParserJson.java
@@ -20,6 +20,7 @@ package com.insatoulouse.chatsystem.model.network.dao;
 
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.insatoulouse.chatsystem.exception.PacketException;
@@ -42,6 +43,10 @@ public class PacketParserJson implements PacketParser {
      * ObjectMapper is useful to serialize (and deserialize) an Packet in JSON
      */
     private ObjectMapper mapper = new ObjectMapper();
+
+    public PacketParserJson() {
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    }
 
     @Override
     public Packet read(String data) throws PacketException {
